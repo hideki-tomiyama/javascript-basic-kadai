2ed30fca7f7374865ebaaeec4a09ef7a0bc06a2c
hideki-tomiyama <149802016+hideki-tomiyama@users.noreply.github.com>
2023-12-18 15:59:10 +0900

    kadai_020提出(再提出)

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,6 +1,7 @@
 //HTML要素から取得、定数へ代入
 const textBtn = document.getElementById("btn");
+const textdisp = document.getElementById("text");
 //クイックされた時のイベント処理
 textBtn.addEventListener("click", () =>{
-  text.textContent = "ボタンをクリックしました";
+  textdisp.textContent = "ボタンをクリックしました";
 })
